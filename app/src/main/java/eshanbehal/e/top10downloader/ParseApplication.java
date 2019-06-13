30cbb3ae9898a836dce5a4711074d62a11676b0a
eshan786 <eshan786behal@gmail.com>
2019-06-13 11:46:11 +0530

    code added for while loop.

--- app/src/main/java/eshanbehal/e/top10downloader/ParseApplication.java
+++ app/src/main/java/eshanbehal/e/top10downloader/ParseApplication.java
@@ -1,5 +1,7 @@
 package eshanbehal.e.top10downloader;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -22,7 +24,7 @@ public class ParseApplication {
 
     public boolean parse(String xmlData) {
         boolean status = true;
-        FeedEntry currentRecord;
+        FeedEntry currentRecord = null;
         boolean inEntry = false;
         String textValue = "";
 
@@ -33,6 +35,50 @@ public class ParseApplication {
             xpp.setInput(new StringReader(xmlData));
             int eventType = xpp.getEventType();
             while(eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = xpp.getName();
+                switch (eventType){
+
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: starting tag for : " + tagName);
+                        if ("entry".equalsIgnoreCase(tagName)){
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+                        break;
+                        
+                    case XmlPullParser.TEXT:
+                        textValue = xpp.getText();
+                        break;
+                        
+                        case XmlPullParser.END_TAG:
+                            Log.d(TAG, "parse: Ending tag for : " + tagName);
+                            if (inEntry){
+                                if ("entry".equalsIgnoreCase(tagName)){
+                                    applications.add(currentRecord);
+                                    inEntry = false;
+                                }
+                                else if ("name".equalsIgnoreCase(tagName)){
+                                    currentRecord.setName(textValue);
+                                }
+                                else if ("artist".equalsIgnoreCase(tagName)){
+                                    currentRecord.setName(textValue);
+                                }
+                                else if ("releaseDate".equalsIgnoreCase(tagName)){
+                                    currentRecord.setName(textValue);
+                                }
+                                else if ("summary".equalsIgnoreCase(tagName)){
+                                    currentRecord.setName(textValue);
+                                }
+                                else if ("image".equalsIgnoreCase(tagName)){
+                                    currentRecord.setName(textValue);
+                                }
+                            }
+                            break;
+                        
+                        default:
+                            //nothing else to do.
+                }
+                eventType = xpp.next();
 
             }
 
